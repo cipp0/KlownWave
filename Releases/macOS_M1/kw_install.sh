bfe5348796e9855b99a29fcb757fb9a4bf2651c8
Francesco Casanova <francescocasanova@outlook.com>
2022-03-10 00:47:54 +0100

    added install

---
+++ Releases/macOS_M1/kw_install.sh
@@ -0,0 +1,10 @@
+echo -n "downloading Csound ... "
+curl -fSsL https://github.com/cipp0/KlownWave/raw/main/Releases/macOS_M1/Csound.zip -o Csound.zip || exit 255
+echo "done"
+echo -n "downloading KlownWave ... "
+curl -fSsL https://github.com/cipp0/KlownWave/raw/main/Releases/macOS_M1/KlownWave.zip -o KlownWave.zip || exit 255
+echo "done"
+echo "moving contents ... "
+unzip -qq -o -d /Library/Audio/Plug-Ins/VST KlownWave.zip || exit 255
+unzip -qq -o -d /Library/Frameworks Csound.zip || exit 255
+echo "all done!"
