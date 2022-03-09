6269f2d920b71a4ffc1506444f014c0a28bfdd27
Francesco Casanova <francescocasanova@outlook.com>
2022-03-10 00:52:38 +0100

    Update kw_install.sh

--- Releases/macOS_M1/kw_install.sh
+++ Releases/macOS_M1/kw_install.sh
@@ -4,7 +4,7 @@ echo "done"
 echo -n "downloading KlownWave ... "
 curl -fSsL https://github.com/cipp0/KlownWave/raw/main/Releases/macOS_M1/KlownWave.zip -o KlownWave.zip || exit 255
 echo "done"
-echo "moving contents ... "
+echo -n "moving contents ... "
 unzip -qq -o -d /Library/Audio/Plug-Ins/VST KlownWave.zip || exit 255
 unzip -qq -o -d /Library/Frameworks Csound.zip || exit 255
-echo "all done!"
+echo "done"
