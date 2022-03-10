7f90f1b8ec1c4fd69abf684d1d6d1676a18730cd
Francesco Casanova <francescocasanova@outlook.com>
2022-03-10 12:50:09 +0100

    added script

--- Releases/macOS_M1/kw_install.sh
+++ Releases/macOS_M1/kw_install.sh
@@ -1,10 +1,21 @@
-echo -n "downloading Csound ... "
-curl -fSsL https://github.com/cipp0/KlownWave/raw/main/Releases/macOS_M1/Csound.zip -o Csound.zip || exit 255
-echo "done"
-echo -n "downloading KlownWave ... "
-curl -fSsL https://github.com/cipp0/KlownWave/raw/main/Releases/macOS_M1/KlownWave.zip -o KlownWave.zip || exit 255
-echo "done"
-echo -n "moving contents ... "
-unzip -qq -o -d /Library/Audio/Plug-Ins/VST KlownWave.zip || exit 255
-unzip -qq -o -d /Library/Frameworks Csound.zip || exit 255
-echo "done"
+vstdir=/Library/Audio/Plug-Ins/VST
+libdir=/Library/Frameworks
+repo_m1="https://github.com/cipp0/KlownWave/raw/main/Releases/macOS_M1"
+repo_x86="https://github.com/cipp0/KlownWave/raw/main/Releases/macOS_x86"
+pkgs="Csound.tar.gz KlownWave.tar.gz"
+
+function die {
+  echo "$1" && exit 255
+}
+
+[ "$(uname -m)" == "arm64" ] && echo "arm64 arch detected" && repo=${repo_m1}
+[ "$(uname -m)" == "x86_64" ] && echo "x86_64 arch detected" && repo=${repo_x86}
+
+for pkg in ${pkgs}; do
+  curl -fSsL "${repo}/${pkg}" -o "${pkg}" || die "error downloading ${pkg}"
+done
+
+tar -C ${libdir} -xzpf Csound.tar.gz || die "error moving framework"
+tar -C ${vstdir} -xzpf KlownWave.tar.gz || die "error moving VST"
+
+echo "all done!"
